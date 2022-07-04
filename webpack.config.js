3ceb675cdc4dc6bfd74dcff7210b6ca740542590
Duane Johnson <duane.johnson@gmail.com>
2022-07-03 18:37:06 -0600

    Remove typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,6 @@ module.exports = {
   mode: "development",
 
   entry: {
-    bundle: ["./src/index.ts"],
+    bundle: ["./src/index.js"],
   },
-
-  output: {
-    filename: "[name].js",
-    path: __dirname + "/dist",
-    publicPath: "/",
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-
-  optimization: {},
-  devtool: false,
 };
